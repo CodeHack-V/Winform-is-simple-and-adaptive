c86572cc5cd38280031b377c04f5c77fd38f2132
CodeHack-V <201173560@qq.com>
2021-02-25 14:41:02 +0800

    Update FResize.cs

--- WindowsFormsApp1/WindowsFormsApp1/FResize.cs
+++ WindowsFormsApp1/WindowsFormsApp1/FResize.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using static System.Windows.Forms.Control;
+using System.Windows.Forms.Control;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
