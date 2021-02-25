63314d0721eb698c36ca6709ab08a16a31cee743
CodeHack-V <201173560@qq.com>
2021-02-25 14:27:24 +0800

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/FResize.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using static System.Windows.Forms.Control;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class FormInitSize
+    {
+        public static double Width = 0.00;
+        public static double Height = 0.00;
+    }
+    public class ControlsSize
+    {
+        public string Name;
+        public double Width;
+        public double Top;
+        public double Height;
+        public double Left;
+    }
+    class ResizeMain
+    {
+        public static Form1 form1 = null;
+        public static List<ControlsSize> controlsSizes = new List<ControlsSize>();
+        public static void SizeInit()
+        {
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                if (form1 != null)
+                {
+                    if (controlsSizes.Count == 0)
+                    {
+                        foreach (Control control in form1.Controls)
+                        {
+                            ControlsSize controlsSize = new ControlsSize();
+                            controlsSize.Name = control.Name;
+                            controlsSize.Width = control.Width;
+                            controlsSize.Height = control.Height;
+                            controlsSize.Top = control.Top;
+                            controlsSize.Left = control.Left;
+                            controlsSizes.Add(controlsSize);
+                        }
+                    }
+                    else
+                    {
+                        controlsSizes.ForEach(CT =>
+                            {
+                                Control[] CurrentControl = form1.Controls.Find(CT.Name, true);
+                                if (CurrentControl.Length >= 1)
+                                {
+                                    foreach (Control control in CurrentControl)
+                                    {
+                                        control.Invoke(new Action(() =>
+                                        {
+                                            control.Width = Convert.ToInt32(CT.Width / FormInitSize.Width * form1.Width);
+                                            control.Height = Convert.ToInt32(CT.Height / FormInitSize.Height * form1.Height);
+                                            control.Top = Convert.ToInt32(CT.Top / FormInitSize.Height * form1.Height);
+                                            control.Left = Convert.ToInt32(CT.Left / FormInitSize.Width * form1.Width);
+                                        }));
+                                    }
+                                }
+                            });
+                    }
+                }
+            }).Start();
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            ResizeMain.SizeInit();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            ResizeMain.form1 = this;
+            FormInitSize.Width = this.Width;
+            FormInitSize.Height = this.Height;
+            ResizeMain.SizeInit();
+        }
+    }
+}
